746e6b620b78ca3673f642320bf4d185cbc12496
onatcipli <onatcipli@outlook.com>
2018-04-21 13:17:23 +0300

    try to commit on inteliji

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //Trying to commit on Inteliji
         System.out.println("Hello World!");
 
         layerMySql sql = new layerMySql();
